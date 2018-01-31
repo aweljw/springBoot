9284a2d9c0062b128a7bc0092bc1f443abe9b230
JiWoong Lee <jiwoong.lee@emotion.co.kr>
2018-01-31 15:15:27 +0900

    springSecurityInmemory 프로젝트 추가

---
+++ springSecurityInmemory/src/main/java/com/springSecurityInmemory/SpringSecurityInmemoryApplication.java
@@ -0,0 +1,12 @@
+package com.springSecurityInmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInmemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInmemoryApplication.class, args);
+	}
+}
